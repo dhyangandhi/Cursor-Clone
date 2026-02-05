3e057631c76a08819a66b1fcb71beba0c53b0bfe
DhyanGandhi <dhyangandhi2008@gmail.com>
2026-02-05 23:54:50 +0530

    02: ATUH

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const X = () => {
+  return(
+  <div>
+      <Button>
+        Click Me
+      </Button>
+  </div>
+  );
+};
+
+export default X; 
