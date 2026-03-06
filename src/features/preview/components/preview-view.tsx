427797e5284dce88afe29b75e6abd2cc5a53b304
DhyanGandhi <dhyangandhi2008@gmail.com>
2026-03-06 22:25:57 +0530

    14: Web-Conter

---
+++ src/features/preview/components/preview-view.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { useState } from "react";
+import { Allotment } from "allotment";
+import {
+    Loader2Icon,
+    TerminalSquareIcon,
+    AlertTriangleIcon,
+    RefreshCwIcon,
+} from "lucide-react";
+
+import { useWebcontainer } from "../hooks/web-container";
+import { PreviewSettingsPopover } from "@/features/preview/components/previewsettingsPopover";
+import { PerviewTerminal } from "@/features/preview/components/preview-terminal";
+import { Button } from "@/components/ui/button";
+import { useProject } from "@/features/projects/components/hooks/use-projects";
+import { Id } from "@convex/_generated/dataModel";
+
+
+export const PreviewView = ({ projectId }: { projectId: Id<"projects"> }) => {
+    const project = useProject(projectId);
+    const [showTerminal, setShowTerminal] = useState(true);
+
+    const {
+        status, previewUrl, error, restart, terminalOuput
+    } = useWebcontainer({
+        projectId,
+        enabled: true,
+        settings: project?.settings,
+    });
+
+    const isLoading = status === "booting" || status === "installing";
+
+    return (
+        <div className="h-full flex flex-col bg-background">
+            <div className="h-8.75 flex items-center border-b bg-sidebar shrink-0">
+                <Button
+                    size="sm"
+                    variant="ghost"
+                    className="h-full rounded-none"
+                    disabled={isLoading}
+                    onClick={() => setShowTerminal(!showTerminal)}
+                    title=" Restart"
+                >
+                    <RefreshCwIcon className="size-3" />
+                </Button>
+
+                <div className="flex-1 h-full flex items-center px-3 bg-background border-x text-xs text-muted-foreground truncate font-mono">
+                    {isLoading && (
+                        <div className="flex itmes-center gap-1.5">
+                            <Loader2Icon className="size-3 animate-spin" />
+                            {status === "booting" ? "Starting..." : "Installing..."}
+                        </div>
+                    )}
+
+                    {previewUrl && <span className="truncate">{previewUrl}</span>}
+                    {!isLoading && !previewUrl && !error && <span>Ready to preview</span>}
+
+                    <Button 
+
+                    size="sm"
+                    variant="ghost"
+                    className="h-full rounded-none"
+                    title="Toggel terminal"
+                    onClick={() => setShowTerminal((value) => !value)}
+                    >
+                        <TerminalSquareIcon className="size-3" />
+                    </Button>
+                    <PreviewSettingsPopover
+                        projectId={projectId}
+                        initialValues={project?.settings}
+                        onSave={restart}
+                    />
+                </div>
+            </div>
+            <div className="flex-1 min-h-0">
+                <Allotment vertical>
+                    <Allotment.Pane>
+                        {error && (
+                            <div className="size-full flex itmes-center justify-center text-muted-foreground">
+                                <div className="flex flex-col items-center gap-2 max-w-md mx-auto text-center">
+                                    <AlertTriangleIcon className="size-6" />
+                                    <p className="text-sm font-medium">{error}</p>
+                                    <Button size="sm" variant="outline" onClick={restart}>
+                                        <RefreshCwIcon className="size-4"/>
+                                        Restart
+                                    </Button>
+                                </div>
+                            </div>
+                        )}
+
+                        {isLoading && !error && (
+                            <div className="size-full flex itmes-center justify-center text-muted-foreground">
+                                <div className="flex flex-col itmes-center gap-2 max-v-md mx-auto text-center">
+                                    <Loader2Icon className="size-6 animate-spin "/>
+                                    <p className="text-sm font-medium">Installing..</p>
+                                </div>
+                            </div>
+                        )}
+                        {previewUrl && (
+                            <iframe 
+                                src={previewUrl}
+                                className="size-full border-0"
+                                title="Preview"
+                            />                        
+                        )}
+                    </Allotment.Pane>
+                    {showTerminal && (
+                        <Allotment.Pane minSize={100} maxSize={500} preferredSize={200}>
+                            <div className="h-full flex flex-col bg-background border-t">
+                                <div className="h-7 flex items-center px-3 text-xs gap-1.5  text-muted-foreground border-b border-border/50 shrink-0">
+                                    <TerminalSquareIcon className="size-3" />
+                                    Terminal
+                                </div>
+                                <PerviewTerminal output={terminalOuput} />
+                            </div>
+                        </Allotment.Pane>
+                    )}
+                </Allotment>
+            </div>
+        </div>
+    )
+}
